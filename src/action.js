6d2023aead27f0866c470d385bc689e64cab10a3
Dylan Bono <dylanrbono@gmail.com>
2022-12-09 17:04:50 -0500

    add yaml stuff

--- src/action.js
+++ src/action.js
@@ -1,5 +1,24 @@
+const core = require('@actions/core');
+const fs = require('fs');
+// const yaml = require('yaml');
 
 async function run() {
+    console.log("Hello World");
+    const version = core.getInput('version');
+
+    const filePath = path.join(process.env.GITHUB_WORKSPACE, 'pubspec.yaml');
+    const file = fs.readFileSync(filePath, 'utf8');
+
+    console.log(file);
+
+    // const pubspec = yaml.parseDocument(file, {schema: "core"});
+
+    // pubspec.set('version', version);
+
+    // const finalDoc = yaml.stringify(pubspec);
+
+    // fs.writeFileSync(filePath, finalDoc);
+
     console.log("Hello World");
 }
 
