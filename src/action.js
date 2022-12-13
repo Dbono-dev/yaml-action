eb108372c5659481180ff76e46ab986ba7fcc3d2
Bono, Dylan <DYLAN_BONO@homedepot.com>
2022-12-13 13:58:31 -0500

    new yarn build

--- src/action.js
+++ src/action.js
@@ -1,26 +1,23 @@
 const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
-// const yaml = require('yaml');
+const yaml = require('yaml');
 
 async function run() {
-    console.log("Hello World");
     const version = core.getInput('version');
 
-    const filePath = path.join(process.env.GITHUB_WORKSPACE, 'pubspec.yaml');
+    const filePath = path.join(process.env.GITHUB_WORKSPACE, "pubspec.yaml");
     const file = fs.readFileSync(filePath, 'utf8');
 
     console.log(file);
 
-    // const pubspec = yaml.parseDocument(file, {schema: "core"});
+    const pubspec = yaml.parseDocument(file, { schema: "core" });
 
-    // pubspec.set('version', version);
+    pubspec.set('version', version);
 
-    // const finalDoc = yaml.stringify(pubspec);
+    const finalDoc = yaml.stringify(pubspec);
 
-    // fs.writeFileSync(filePath, finalDoc);
-
-    console.log("Hello World");
+    fs.writeFileSync(filePath, finalDoc);
 }
 
 run();
