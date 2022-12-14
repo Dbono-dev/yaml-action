7714bf8a88d67fa9d3c3fe87917fc08638201438
Bono, Dylan <DYLAN_BONO@homedepot.com>
2022-12-14 16:35:47 -0500

    add dev dependencies

--- dist/index.js
+++ dist/index.js
@@ -11121,21 +11121,45 @@ const path = __nccwpck_require__(1017);
 const yaml = __nccwpck_require__(4083);
 
 async function run() {
-    const version = core.getInput('version');
+    const changes = core.getInput('changes');
+    const commentOutDevDependencies = core.getInput('commentOutDevDependencies');
+
+    const allChanges = new Map(changes);
 
     const filePath = path.join(process.env.GITHUB_WORKSPACE, "/pubspec.yaml");
 
-    const file = fs.readFileSync(filePath, 'utf8');
+    var file = fs.readFileSync(filePath, 'utf8');
+
+    if (commentOutDevDependencies == 'true') {
+        const listOfPubspec = file.split('\n');
+
+        for (var i = 0; i < listOfPubspec.length; i++) {
+            if (listOfPubspec[i] == 'dependency_overrides:') {
+                for (var a = i; a < listOfPubspec.length; a++) {
+                    if (listOfPubspec[a] == 'dependencies:') {
+                        break;
+                    }
+                    listOfPubspec[a] = '#' + listOfPubspec[a];
+                }
+            }
+        }
+
+        file = listOfPubspec.join('\n');
+    }
 
     const pubspec = yaml.parseDocument(file, { schema: "core" });
 
-    pubspec.set('version', version);
+    allChanges.forEach(
+        function (value, key) {
+            const path = key.split('.');
+            pubspec.setIn(path, value);
+            console.log(key + ' set to: ' + value);
+        },
+    );
 
     const finalDoc = yaml.stringify(pubspec);
 
     fs.writeFileSync(filePath, finalDoc);
-
-    console.log('Version Number Updated to: ' + version);
 }
 
 run();
--- src/action.js
+++ src/action.js
@@ -4,21 +4,45 @@ const path = require('path');
 const yaml = require('yaml');
 
 async function run() {
-    const version = core.getInput('version');
+    const changes = core.getInput('changes');
+    const commentOutDevDependencies = core.getInput('commentOutDevDependencies');
+
+    const allChanges = new Map(changes);
 
     const filePath = path.join(process.env.GITHUB_WORKSPACE, "/pubspec.yaml");
 
-    const file = fs.readFileSync(filePath, 'utf8');
+    var file = fs.readFileSync(filePath, 'utf8');
+
+    if (commentOutDevDependencies == 'true') {
+        const listOfPubspec = file.split('\n');
+
+        for (var i = 0; i < listOfPubspec.length; i++) {
+            if (listOfPubspec[i] == 'dependency_overrides:') {
+                for (var a = i; a < listOfPubspec.length; a++) {
+                    if (listOfPubspec[a] == 'dependencies:') {
+                        break;
+                    }
+                    listOfPubspec[a] = '#' + listOfPubspec[a];
+                }
+            }
+        }
+
+        file = listOfPubspec.join('\n');
+    }
 
     const pubspec = yaml.parseDocument(file, { schema: "core" });
 
-    pubspec.set('version', version);
+    allChanges.forEach(
+        function (value, key) {
+            const path = key.split('.');
+            pubspec.setIn(path, value);
+            console.log(key + ' set to: ' + value);
+        },
+    );
 
     const finalDoc = yaml.stringify(pubspec);
 
     fs.writeFileSync(filePath, finalDoc);
-
-    console.log('Version Number Updated to: ' + version);
 }
 
 run();
